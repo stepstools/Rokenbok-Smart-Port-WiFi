9cdce969f9e02587dadbede8bb4548cb3a0e424b
Jeremy Schwartz <stepstools@gmail.com>
2024-07-06 17:12:30 -0400

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,6 +1,5 @@
 // ASCII Art: https://www.patorjk.com/software/taag/ ("Big" Font)
 // OTA Update Code Shamelessly Stolen From: https://github.com/Jeija/esp32-softap-ota/tree/master/main
-// TEST COMMIT
 
 #include <stdio.h>
 #include <string.h>
